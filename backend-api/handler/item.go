812076a5ae4357ca802f262b0a3f31a77985c5d9
Yasukastsu.A <handbuuuu@gmail.com>
2020-01-31 01:13:10 +0900

    add handler

---
+++ backend-api/handler/item.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"vue-golang-payment-app/backend-api/db"
+)
+
+// GetLists - get all items
+func GetLists(c Context) {
+	res, err := db.SelectAllItems()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+// GetItem - get item by id
+func GetItem(c Context) {
+	identifer, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	res, err := db.SelectItem(int64(identifer))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	c.JSON(http.StatusOK, res)
+}
