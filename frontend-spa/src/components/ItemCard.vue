48eb325d96df7cc75de673fcff7414738da77113
Yasukastsu.A <handbuuuu@gmail.com>
2020-02-14 00:08:06 +0900

    add ItemCard.vue

---
+++ frontend-spa/src/components/ItemCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="itemcard">
+        <h1>{{ item.Name }}</h1>
+        <h2>{{ item.Description }}</h2>
+        <h2>{{ item.Amout }}円</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ItemCard',
+    props: [
+        'item'
+    ]
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: aqua;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+.itemcard {
+    border: solid 1px gray;
+}
+</style>
